c7b60937425eac68a00ce6bda813958876a7112a
Anthony Wesley <awl-7@live.com>
2023-11-14 22:48:49 -0300

    new config deploy

--- src/components/Expenses/index.tsx
+++ src/components/Expenses/index.tsx
@@ -1,16 +1,16 @@
 "use client";
 import { useAppContext } from "@/context";
 import { categories } from "@/data/categories";
-import { ItemType } from "@/type/ItemType";
-import { Table, TableBody, TableCell, TableRow } from "../ui/table";
 import { FormattedCurrency } from "@/helpers/others";
+
+import { Chart } from "react-google-charts";
 
 export default function Expenses() {
   const { filteredList } = useAppContext();
   const categoriesArray = Object.values(categories);
 
   const sunCategoryValues = () => {
-    let newCategory: { category: string; value: number }[] = [];
+    let newCategory: { category: string; value: number; color: string }[] = [];
 
     if (filteredList) {
       filteredList.forEach((item) => {
@@ -23,7 +23,11 @@ export default function Expenses() {
         if (existingCategoryIndex !== -1) {
           newCategory[existingCategoryIndex].value += value;
         } else {
-          newCategory.push({ category, value });
+          newCategory.push({
+            category: item.category,
+            value,
+            color: categories[item.category].color,
+          });
         }
       });
     }
@@ -31,25 +35,32 @@ export default function Expenses() {
     return newCategory;
   };
 
-  console.log(sunCategoryValues());
+  const dataWithoutHeader = sunCategoryValues().map((item) => [
+    `${categories[item.category].title} - ${FormattedCurrency(item.value)}`,
+    // FormattedCurrency(item.value),
+    item.value,
+  ]);
+
+  const data = [["Task", "Hours per Day"], ...dataWithoutHeader];
+  const options = {
+    height: 420,
+    tooltip: { text: "percentage" },
+    legend: {
+      position: "none",
+    },
+
+    slices: sunCategoryValues().map((item) => ({
+      color: item.color,
+    })),
+  };
 
   return (
-    <Table className="bg-white rounded-lg">
-      <TableBody className="flex flex-col justify-center items-end">
-        {sunCategoryValues().map((item, index) => (
-          <TableRow key={index}>
-            <TableCell
-              className="rounded-lg text-white w-[100px]"
-              style={{ backgroundColor: categories[item.category]?.color }}
-            >
-              {categories[item.category].title}
-            </TableCell>
-            <TableCell className="w-[100px] text-red-500">
-              {FormattedCurrency(item.value)}
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      // height={"400px"}
+    />
   );
 }
